9a85cc4746e27b3a35900603035427c98436b739
Luke Rodgers <lukeasrodgers@gmail.com>
2014-03-27 10:56:36 -0400

    Remove code that inserts jammit before_filter into controller. Since we want this to work in environments other than just dev, we'll need to remove this from here and move it into our environment config files.

--- lib/jammit/controller.rb
+++ lib/jammit/controller.rb
@@ -89,9 +89,3 @@ end
 
 # Make the Jammit::Controller available to Rails as a top-level controller.
 ::JammitController = Jammit::Controller
-
-if defined?(Rails) && Rails.env.development?
-  ActionController::Base.class_eval do
-    append_before_filter { Jammit.reload! }
-  end
-end
